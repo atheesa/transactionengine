cf55465d6ad94a63184539fc4cb61e8f9ef9c5b8
atheesa <nantheesanr@gmail.com>
2026-01-17 20:17:38 +0000

    first commit

---
+++ src/main/java/com/transactionengine/core/TransactionengineApplication.java
@@ -0,0 +1,13 @@
+package com.transactionengine.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transactionengine/core/TransactionengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionengine.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
