a33b05c7048c0135e8ebfacd609a452892cc36b7
atheesa <nantheesanr@gmail.com>
2026-02-04 06:03:43 +0000

    add basic Spring Security infrastructure

--- src/main/java/com/transactionengine/core/controller/TransactionController.java
+++ src/main/java/com/transactionengine/core/controller/TransactionController.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.transactionengine.core.config.SecurityConfig;
 import com.transactionengine.core.dto.TransferRequest;
+import com.transactionengine.core.model.Role;
 import com.transactionengine.core.model.Transaction;
+import com.transactionengine.core.model.User;
+import com.transactionengine.core.repository.RoleRepository;
+import com.transactionengine.core.repository.UserRepository;
 import com.transactionengine.core.service.TransactionService;
 
 import jakarta.validation.Valid;
@@ -23,11 +30,18 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/transactions")
 public class TransactionController {
     
-
+    private final RoleRepository roleRepository;
     private final TransactionService transactionService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TransactionController(TransactionService transactionService){
+
+
+    public TransactionController(TransactionService transactionService, RoleRepository roleRepository,UserRepository userRepository,PasswordEncoder passwordEncoder){
         this.transactionService = transactionService;
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // Create Transaction
@@ -62,10 +76,37 @@ public class TransactionController {
     
     // Create basic test accounts
     @PostMapping("/setup")
-    public String setupAccounts() {
+    public String setupTestInit() {
+
+        Role roleUser = roleRepository.findByName("ROLE_USER").orElseGet(() -> roleRepository.save(new Role("ROLE_USER")));
+        Role roleAdmin = roleRepository.findByName("ROLE_ADMIN").orElseGet(() -> roleRepository.save(new Role("ROLE_ADMIN")));
+
         transactionService.createAccount("ACC-100", "Alice", "USD");
         transactionService.createAccount("ACC-200", "Bob", "USD");
-        return "Success Created";
+
+       if(userRepository.findByUsername("alice").isEmpty()) {
+            User alice = new User(
+                "alice",
+                passwordEncoder.encode("test_password_1"),
+                Set.of(roleUser,roleAdmin)
+            );
+            userRepository.save(alice);
+       }
+        
+ 
+       if(userRepository.findByUsername("bob").isEmpty()) {
+            User bob = new User(
+                "bob",
+                passwordEncoder.encode("test_password_2"),
+                Set.of(roleUser,roleAdmin)
+            );
+            userRepository.save(bob);
+       }       
+        
+        
+        
+
+        return "System Initialized: Roles, Users, and Accounts created";
     }
     
 
